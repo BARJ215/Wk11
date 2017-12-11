e89b2023fa7133de8876ed4ff0f8d616eccfc05a
BARJ215 <barj2_15@uni.worc.ac.uk>
2017-12-11 14:51:07 +0000

    CameraTemplate

--- www/js/index.js
+++ www/js/index.js
@@ -1,121 +1,23 @@
-//global variables
-var filetext  = "";
-var fileBinding;
-var fileEntry;
 
-console.log("setting up events");
+var destinationType; //used sets what should be returned (image date OR file path to image for example)
 
-//setup event listeners
-$(document).on("pagecreate","#pageone", onPageCreated);
-
-//setup listener for device API load
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// once jQuery page 'pageone' has been created 
-function onPageCreated() {
-
-     console.log("page created");
-	
-	//setup buttons
-	$('#writeFile').on("click", writeFile);
-	$('#deleteFile').on("click", deleteFile);
-	
-	// setup RactiveJS binding
-
-	//binding between variable 'filetext' and the template 
-	fileBinding = new Ractive({
-		el: 'container',
-		template: '#template',
-		data: { filetext: filetext}
-	});
-
-
-	//detects changes in the text box and updates the 'filetext' value with the new value
-	fileBinding.observe( 'filetext', function ( newValue, oldValue ) {
-  		filetext = newValue; 
-	});
-    
-}
+document.addEventListener("deviceready",onDeviceReady,false);
 
 function onDeviceReady() {
-	console.log("device ready");
-	//setup access to filesystem
-	//window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-    window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, gotFS, fail);
+	destinationType=navigator.camera.DestinationType;
 }
 
-//get access to file and CREATE if does not exists
-function gotFS(fileSystem) {
-    
- 	fileSystem.getFile("test.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+function capturePhoto() {
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+	destinationType: destinationType.DATA_URL });
 }
-
-//get file entry
-function gotFileEntry(fileEntry) {
-	console.log("got file entry");
-	this.fileEntry = fileEntry
-	fileEntry.file(gotFile, fail);
-}
-
-//get file itself
-function gotFile(file){
-    console.log("got file");
-	readAsText(file);
-}
-
-//READ text from file - assumes that the file contains 
-function readAsText(file) {
-    console.log("readAsText");
 	
-	var reader = new FileReader();
-	
-	//assigns a callback function to be run once the file has been completely read
-	reader.onloadend = function(evt) {
-	
-		//store the new string in 'filetext'
-		filetext = evt.target.result;
-		
-		//update the binding 
-		fileBinding.set({ filetext: filetext });
-    };
-	
-	//begin reading the file
-   	reader.readAsText(file);
+function onPhotoDataSuccess(imageData) {
+	var image = document.getElementById('image');
+	image.style.display = 'block';
+	image.src = "data:image/jpeg;base64," + imageData;
 }
 
-
-//UDPATE file contents - called when submit button is pressed
-function writeFile()
-{
-    console.log("writeFile: "  + fileEntry.fullPath);
-    
-	fileEntry.createWriter(
-		function (writer) { 
-			writer.write(filetext);
-		}, 
-		fail
-	);
+function onFail(message) {
+      alert('Failed because: ' + message);
 }
-
-//DELETE file
-function deleteFile()
-{
-    
-    console.log("deleteFile");
-	
-	fileEntry.remove(
-		function () {
-			alert("File deleted");
-            onDeviceReady();
-		}, 
-		fail
-	);
-	
-	
-}
-
-function fail(error) {
-	alert("Cannot use file: " + error.message);
-}
-
-
